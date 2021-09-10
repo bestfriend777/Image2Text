821d7819bd5e999fd8a9395d6e80b347de198559
bestfriend777 <59232292+bestfriend777@users.noreply.github.com>
2021-09-09 22:17:01 -0400

    added image saving

---
+++ Image2Text/src/Printer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Printer // Printer is written using the Singleton technique... Its a super useful design
+{
+    static Printer m_Instance; // m_Instance acts as the access point to the Printer class and Methods
+
+    private Printer() // constructor not accessible. ...also private keyword not needed, private by default.
+    {
+    }
+
+    public static Printer Instance()
+    {
+        // the constructor is only ever called from within the class and only if a Printer instance doesnt exist already
+        // this is the a core part of the singleton design along with the m_Instance object
+        if (m_Instance == null)
+            m_Instance = new Printer();
+        return m_Instance;
+    }
+
+    // shorter syntax than Printer.Print()?
+    public static void Print(String str)
+    {
+        Console.WriteLine(str);
+    }
+}
--- Image2Text/src/Program.cs
+++ Image2Text/src/Program.cs
@@ -5,55 +5,49 @@ public class Program
 {
     static int Main(string[] args)
     {
-
         Image img;
         String solutionDir = "D:/work/dev/c#/Image2Text/";
         String projectDir = solutionDir + "Image2Text/";
+        String imgDir = projectDir + "res/img/";
 
-        Printer.Print("enter the img path (relative to the project directory)\n");
-        String str = Console.ReadLine();
         try
         {
-            //img = Image.FromFile(projectDir + "res/img/chix.bmp");
+            Printer.Print("enter the img path relative to the project directory:");
+            String str = Console.ReadLine();
             img = Image.FromFile(projectDir + str);
-            Printer.Print("Path: " + projectDir + "res/img/chix.bmp\nImage Loaded\n");
+            Printer.Print("\nImage Loaded!" );
         }
         catch
         {
-            Printer.Print("Path: " + projectDir + "res/img/chix.bmp\nImage Load Failed\n");
+            Printer.Print("\nLoad Failed!");
             return Exit();
         }
 
+        Printer.Print("Save as new copy? Y/N");
+        if (Console.ReadLine().ToUpper() == "Y")
+        {
+            Printer.Print("\nEnter Name: ");
+            String name = Console.ReadLine();
+
+            try
+            {
+                img.Save(imgDir + name);
+                Printer.Print("\n'" + name + "' Saved as New Image!");
+            }
+            catch
+            {
+                Printer.Print("\nSave Failed!");
+                return Exit();
+            }
+        }
+        
         return Exit();
     }
 
     static int Exit()
     {
-        Printer.Print("Press any key to exit.");
+        Printer.Print("\nPress ~any~ key to exit");
         Console.ReadKey();
         return 0;
-    }
-}
-
-public class Printer
-{
-    static Printer m_Instance; // access point to the static Printer
-
-    private Printer() // constructor not accessible private keyword not needed, private by default
-    {
-    }
-
-    public static Printer Instance()
-    {
-        // the constructor is only called privately within the class and only if a Printer instance doesnt exist already
-        if (m_Instance == null)
-            m_Instance = new Printer(); 
-        return m_Instance;
-    }
-    
-    // Come up with shorter syntax than 'Printer.Print()'
-    public static void Print(String str)
-    {
-        Console.WriteLine(str);
     }
 }
